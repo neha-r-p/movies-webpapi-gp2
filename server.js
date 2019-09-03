6ee89ce180d3c0e76c2e137c5929358732afe5a4
Neha Patel <neha.r.p@gmail.com>
2019-09-03 11:13:52 -0700

    updated from Luis files

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json()); // teaches express to parse JSON body
+let movieId = 5;
+let actorId = 3;
 let actors = [
   {
     id: 1,
@@ -52,12 +55,40 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'up...' });
 });
 
+// test query string: localhost:8000/api/movies?minrating=3
 server.get('/api/movies', (req, res) => {
-  res.status(200).json(movies);
-  
+  const minRating = req.query.minrating;
+
+  let result = [...movies];
+
+  // if the client provides a minrating, filter the response
+  if (minRating) {
+    result = movies.filter(m => m.rating >= minRating);
+  }
+
+  res.status(200).json(result);
 });
 
+server.post('/api/movies', (req, res) => {
+  const movie = req.body;
 
+  // add the new id
+  movie.id = movieId++;
+  movies.push(movie);
+
+  // return correct http status code for operation
+  res.status(201).json(movies);
+});
+
+server.delete('/api/movies/:id', (req, res) => {
+  const id = req.params.id;
+
+  movies = movies.filter(m => m.id !== Number(id));
+
+  res.status(200).json(movies);
+});
+
+// as user of this api, I want and endpoint to see a list of actors, so that I can see all the actors
 
 // export default server; // ES2015 modules
 // module.exports = { server }; // CommonJS modules (node)
